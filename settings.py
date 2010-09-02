c36d93f25cf426fd8720ef7c5514de8c397d8e89
forman.simon <forman.simon@d31ca306-d618-6f8a-1d71-b0ba20e69f45>
2010-09-02 17:17:25 +0000

    Activate auth system and use sqlite db.

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 # Django settings for lrbserv project.
-
+import os
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -9,12 +9,15 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = ''           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = ''             # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+
+BASE_PATH = '/home/tc/Software/lrbserv'
+
+def base(path):
+    return os.path.join(BASE_PATH, path)
+
+
+DATABASE_ENGINE = 'sqlite3'
+DATABASE_NAME = base('test.db')
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
