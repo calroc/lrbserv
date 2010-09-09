895a82f42f02734bfd81ad21c47094c560bd7930
forman.simon <forman.simon@d31ca306-d618-6f8a-1d71-b0ba20e69f45>
2010-09-09 19:28:17 +0000

    Let's try ForeignKey instead of OneToOneField.

--- core/models.py
+++ core/models.py
@@ -5,8 +5,8 @@ from django.contrib.auth.models import User, permalink
 class UserProfile(models.Model):
     greeting = models.CharField(max_length=128)
     image_file = models.CharField(max_length=128)
-#    user = models.ForeignKey(User, unique=True)
-    user = models.OneToOneField(User)
+    user = models.ForeignKey(User, unique=True)
+#    user = models.OneToOneField(User)
 
     @permalink
     def get_absolute_url(self):
