ddabb77f7ddd77d686a964d9688403052d681fcc
forman.simon <forman.simon@d31ca306-d618-6f8a-1d71-b0ba20e69f45>
2010-09-09 20:28:15 +0000

    Set only lowercase tags.

--- settings.py
+++ settings.py
@@ -29,6 +29,9 @@ ACCOUNT_ACTIVATION_DAYS = 7
 
 AUTH_PROFILE_MODULE = 'core.UserProfile'
 
+# Let's not mess with uppercase letters in tags.
+FORCE_LOWERCASE_TAGS = True
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
