1f95250c6e78da6bc2c4f56616f06b3449eb4728
forman.simon <forman.simon@d31ca306-d618-6f8a-1d71-b0ba20e69f45>
2010-09-09 19:21:07 +0000

    Still activating user profiles.

--- trunk/core/models.py
+++ trunk/core/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, permalink
 
 
 class UserProfile(models.Model):
@@ -7,3 +7,11 @@ class UserProfile(models.Model):
     image_file = models.CharField(max_length=128)
 #    user = models.ForeignKey(User, unique=True)
     user = models.OneToOneField(User)
+
+    @permalink
+    def get_absolute_url(self):
+        return (
+            'profiles_profile_detail',
+            (),
+            dict(username=self.user.username),
+            )
